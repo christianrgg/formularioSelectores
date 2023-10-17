9de00cbe59d999df78ed28ab2543b91b65f628f7
Christian Roberto <christian.roberto.g@gmail.com>
2023-10-16 19:17:16 -0600

    Estructura de directorios para esta aplicación

---
+++ src/app/country/countries.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SelectorPagesComponent } from './pages/selector-pages/selector-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    SelectorPagesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountriesModule { }
