892b68e7e4b3c0984fe0c105e2704b0094dfd6cc
Christian Roberto <christian.roberto.g@gmail.com>
2023-10-21 13:56:48 -0600

    Selector de regiones

--- src/app/country/pages/selector-pages/selector-pages.component.ts
+++ src/app/country/pages/selector-pages/selector-pages.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {CountriesService} from "../../services/countries.service";
+import {Region} from "../../interfaces/country.interfaces";
 
 @Component({
   templateUrl: './selector-pages.component.html',
@@ -16,6 +18,10 @@ export class SelectorPagesComponent {
 
   constructor(
     private fb: FormBuilder,
-  ) {
+    private countriesService: CountriesService,
+  ) {}
+
+  get regions():Region[]{
+    return  this.countriesService.regions;
   }
 }
